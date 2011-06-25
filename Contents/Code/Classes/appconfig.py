ac4aedbab8605ebba167d4dc1c91e295ab83736c
sander1 <sanderspies+plexapp@gmail.com>
2011-06-25 02:24:16 +0200

    first commit

---
+++ Contents/Code/Classes/appconfig.py
@@ -0,0 +1,7 @@
+project = "goanimate"
+channel = "plex"
+video_format = "1"
+video_type = "video/mp4" #media_path for old default
+app_title = 'GoAnimate'
+app_subtitle = "Make your own animated videos"
+app_summary = 'GoAnimate is a fun app that lets you make animated videos, for free, in just 10 minutes, without having to draw. You can even create your own cast of characters. There are millions of people using GoAnimate. Try it out now!'
